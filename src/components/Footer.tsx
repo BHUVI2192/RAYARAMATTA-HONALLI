5a30ddce27653cf14d239250b442c379e7f80595
BHUVAN <cnbhuvan011@gmail.com>
2026-05-24 16:50:42 +0530

    Add Facebook icon and link in footer Contact Us section

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -53,6 +53,19 @@ export const Footer: React.FC = () => {
                   @srs_matha_honnali
                 </a>
               </li>
+              <li className="flex gap-3 text-white">
+                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="currentColor" className="text-yellow-500 shrink-0 mt-0.5">
+                  <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z" />
+                </svg>
+                <a
+                  href="https://www.facebook.com/srsmathahonnali"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="hover:text-yellow-500 transition-colors font-medium"
+                >
+                  srsmathahonnali
+                </a>
+              </li>
             </ul>
           </div>
 
